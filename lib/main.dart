80c74d1bc161837c7a6d83fe91f2a2939b13a018
karim <karimkhaled345444@gmail.com>
2022-06-30 18:17:04 +0200

    last update karim1

--- lib/main.dart
+++ lib/main.dart
@@ -15,63 +15,51 @@ import 'package:signup_demo/providers/notifiers/subcategory_notifier.dart';
 import 'package:signup_demo/providers/theme_provider.dart';
 import 'component/local/theme.dart';
 
-
-void main()async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPref.init();
 
   runApp(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create:(context) => AppProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => AppProvider(),
         ),
-          ChangeNotifierProvider(
-          create:(context) => ThemeProvider(),
+        ChangeNotifierProvider(
+          create: (context) => ThemeProvider(),
         ),
-          ChangeNotifierProvider(
-            create:(context) => CategoryNotifier(),
-          ),
-          ChangeNotifierProvider(
-            create:(context) => BooksNotifier()
-          ),
-          ChangeNotifierProvider(
-            create:(context) => LogInNotifier()
-          ),
-          ChangeNotifierProvider(
-            create:(context) => RequestNotifier()
-          ),
-          ChangeNotifierProvider(
-            create:(context) => StudentNotifier()
-          ),
-          ChangeNotifierProvider(
-              create:(context) => SubCategoriesNotifier()
-          ),
-          ChangeNotifierProvider(
-              create:(context) => FavoritesNotifier()
-          ),
-        ],
-        child: const MyApp(),
-      ),
+        ChangeNotifierProvider(
+          create: (context) => CategoryNotifier(),
+        ),
+        ChangeNotifierProvider(create: (context) => BooksNotifier()),
+        ChangeNotifierProvider(create: (context) => LogInNotifier()),
+        ChangeNotifierProvider(create: (context) => RequestNotifier()),
+        ChangeNotifierProvider(create: (context) => StudentNotifier()),
+        ChangeNotifierProvider(create: (context) => SubCategoriesNotifier()),
+        ChangeNotifierProvider(create: (context) => FavoritesNotifier()),
+      ],
+      child: const MyApp(),
+    ),
   );
 }
 
 class MyApp extends StatelessWidget {
-   const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     ThemeProvider themes = Provider.of<ThemeProvider>(context);
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
-        theme: theme() ,
+        theme: theme(),
         darkTheme: darkTheme(),
-        themeMode:themes.getThemeMode() == true  ? ThemeMode.light : ThemeMode.dark,
+        themeMode:
+            themes.getThemeMode() == true ? ThemeMode.light : ThemeMode.dark,
         home: SharedPref.getBoolean('isBoardLoaded') != true
-            ? OnBoardingScreen() : SharedPref.getBoolean('login') == true
-            ? const MainPage() : const LoginScreen()
-
-      );
-
+            ? OnBoardingScreen()
+            : SharedPref.getBoolean('login') == true
+                ? const MainPage()
+                : const LoginScreen());
   }
 }
